bb79a978f35684801a75ca7a1113499fffea01fe
JaimeAndresMenesesAparicio <jaime.meneses89@gmail.com>
2023-02-17 21:04:56 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-let num1 = Number(prompt(`Ingrese el primer número:`));
-let num2 = Number(prompt(`Ingrese el segundo número:`));
-console.log(`La suma de los 2 números ingresados es: ${num1+num2}`)
+let A = Number(prompt(`Ingrese el valor de la variable A:`));
+let B = Number(prompt(`Ingrese el valor de la variable B:`));
+let C = Number(prompt(`Ingrese el valor de la variable C:`));
+console.log(`El número mayor de las variables es: ${Math.max(A,B,C)}`)
+console.log(`El número mínimo de las variables es: ${Math.min(A,B,C)}`)
