d491a1b8c5269864c28dfed933e1264f17b24d9b
JaimeAndresMenesesAparicio <jaime.meneses89@gmail.com>
2023-02-17 19:06:56 -0500

    Ejercicio 1

---
+++ main.js
@@ -0,0 +1,8 @@
+let num1 = Number(prompt(`Ingrese el primer número:`));
+let num2 = Number(prompt(`Ingrese el segundo número:`));
+if( num1 > num2){
+    console.log(`De los 2 números ingresados el mayor es: ${num1}`)
+}
+else{
+    console.log(`De los 2 números ingresados el mayor es: ${num2}`)
+}
